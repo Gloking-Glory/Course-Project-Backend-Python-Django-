04afd6b029f6086826728e97eda8f2a4331d2cca
Glory <ayanwolaglory@gmail.com>
2025-10-22 11:05:24 +0100

    update settings

--- jeceProject/settings.py
+++ jeceProject/settings.py
@@ -29,7 +29,7 @@ load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ".elasticbeanstalk.com,localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 
 # Static files (CSS, JavaScript, Images)
